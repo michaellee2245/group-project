1c6acfabafe9ccf3a0854d37a79e895c8d54078b
joshparkerj <superfakek@gmail.com>
2019-01-31 22:29:58 -0700

    get api/user/from-team/:id

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const authRouter = require('./auth');
-/*
+
 const userRouter = require('./user');
-const taskRouter = require('./task');
-const miscRouter = require('./misc');
 
 router.use('/user', userRouter);
+/*
+const taskRouter = require('./task');
+const miscRouter = require('./misc');
 router.use('/task', taskRouter);
 router.use('/misc', miscRouter);
 */
