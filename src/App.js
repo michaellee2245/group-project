97c2ecda7c393c0148f0d3c6151e3da940ddbe06
Michael Lee <michaellee2245@gmail.com>
2019-02-02 10:54:26 -0700

    added marketing.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Marketing from './views/marketing/Marketing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           Home
-
+          <Marketing />
       </div>
     );
   }
