8269b12268e6e9305b853fceb44ef4ce75dac1fb
JHall4475 <jhall4475@yaoo.com>
2019-02-05 21:13:10 -0700

    changed nav bar icons and hover

--- src/components/TopNavBar/TopNavBar.js
+++ src/components/TopNavBar/TopNavBar.js
@@ -1,60 +1,73 @@
-import React, {Component} from 'react';
-import bellRinging from '../../assets/bellRinging.png'
-import peopleIcon from '../../assets/TwoPeople.png'
+import React, { Component } from 'react';
+// import bellRinging from '../../assets/bellRinging.png'
+// import peopleIcon from '../../assets/TwoPeople.png'
 import './TopNavBar.scss'
 import magGlass from '../../assets/magnifying-glass.png'
-import addButton from '../../assets/add_blue.png'
+// import addButton from '../../assets/add_blue.png'
 
 class TopNavBar extends Component {
-    state={}
+    state = {
 
-render(){
-    return(
-        <div className="navbar-wrapper">
-            <div className="navbar-tab">
-                <a href="https://youtu.be/dQw4w9WgXcQ">
-                    <span>
-                        <img src={bellRinging} alt="Notifications" className="nav-img"></img>
-                    </span>
-                </a>
-            </div>
-            <div className="navbar-tab">
-                <a href="https://youtu.be/ZZ5LpwO-An4">
-                    <span>
-                        <img src={peopleIcon} alt="Members" className="nav-img"></img>
-                    </span>
+    }
+
+    render() {
+        return (
+            <div className="navbar-wrapper">
+                <div className="navbar-tab">
+                    <a href="https://youtu.be/dQw4w9WgXcQ">
+                        <div className="dropdown-notifs">
+                            {/* <img src={bellRinging} alt="Notifications" className="nav-img"></img> */}
+                            <i className="material-icons">notifications_none</i>
+
+                            <div className="dropdown-content">
+                                Notifications
+                </div>
+                        </div>
                     </a>
-            </div>
-            <div class="search">
-                <span class="search-icon">
-                <img src={magGlass} alt="magGlass" className="search-img"></img>
-                
-                </span> 
-             <input type="searchbar"placeholder="Search Everything ..." className="search-bar">
-             </input>
-            </div>
-
-            <div className="navbar-tab-add">
-                <a href="https://youtu.be/dQw4w9WgXcQ" className="add-text">
-                    <span className="add-member-span">
-                        <img src={addButton} alt="add Members" className="nav-img"></img>
-                       <p> Invite Team Members </p>
-                    </span>
-                </a>
-            </div>
-            
-            <div className="navbar-tab">
-                <a href="https://youtu.be/ZZ5LpwO-An4">
-                    <span>
-                        <img src={peopleIcon} alt="UserProfile" className="nav-img"></img>
-                        UserProfile
-                    </span>
+                </div>
+                <div className="navbar-tab">
+                    <a href="https://youtu.be/ZZ5LpwO-An4">
+                        <div className="dropdown-people">
+                            {/* <img src={peopleIcon} alt="Members" className="nav-img"></img> */}
+                            <i className="material-icons">people_outline</i>
+                            <div className="dropdown-content">
+                                Team Members
+                        </div>
+                        </div>
                     </a>
-            </div>
+                </div>
+                <div className="search">
+                    <span className="search-icon">
+                        <img src={magGlass} alt="magGlass" className="search-img"></img>
 
-        </div>
-    )
-}
+                    </span>
+                    <input type="searchbar" placeholder="Search Everything ..." className="search-bar">
+                    </input>
+                </div>
+
+                <div>
+                    <a href="https://youtu.be/dQw4w9WgXcQ" className="add-text">
+                        <div className="add-member-span">
+                            {/* <img src={addButton} alt="add Members" className="nav-img"></img> */}
+                            <i className="material-icons">add_circle_outline</i>
+                            <p> Invite Team Members </p>
+                        </div>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="https://youtu.be/ZZ5LpwO-An4" className="add-text">
+                        <span className="add-member-span">
+                            {/* <img src={peopleIcon} alt="UserProfile" className="nav-img"></img> */}
+                            <i className="material-icons">face</i>
+                            <p> UserProfile</p>
+                        </span>
+                    </a>
+                </div>
+
+            </div>
+        )
+    }
 
 
 
