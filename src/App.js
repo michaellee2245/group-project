2781f8cab2735a7912df1d88846955fb896e3026
McKay Nilsson <mckay.nilsson@gmail.com>
2019-02-09 10:27:04 -0700

    created why us view

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import history from './history'
 import './App.css';
 import DashboardLanding from './views/dashboard/DashboardLanding';
 import { ConnectedRouter } from 'connected-react-router';
-import {Switch,Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Marketing from './views/marketing/Marketing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ConnectedRouter history = {history}>
+        <ConnectedRouter history={ history }>
           <Switch>
-            <Route path="/dashboard" component = {DashboardLanding} />
-            
-            <Route component={Marketing} />
+            <Route path="/dashboard" component={ DashboardLanding } />
+
+            <Route component={ Marketing } />
           </Switch>
         </ConnectedRouter>
       </div>
-      
+
     );
   }
 }
---
+++ src/views/marketing/MarketWhyUs/MarketWhyUs.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class WhyUs extends Component {
+    render(){
+        return(
+            <div>
+                Why Us
+            </div>
+        )
+    }
+}
+
+export default WhyUs;
