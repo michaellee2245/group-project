94464b8590907deafa3e85c507eb277eef378b7c
JHall4475 <jhall4475@yaoo.com>
2019-02-14 18:33:46 -0700

    merge

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
         </ConnectedRouter> */}
 
         <Inbox></Inbox>
+       
         
       </div>
       
--- src/components/Inbox/Inbox.js
+++ src/components/Inbox/Inbox.js
@@ -1,39 +1,49 @@
 import React, { Component } from 'react';
 import './inbox.scss'
+import DefaultInbox from './DefaultInbox';
 class Inbox extends Component {
 
     state = {
+        toggleDefaultInbox: true,
+        allUpdates: false,
 
     }
 
+    componentDidMount(){
+
+    }
+
+    allUpdateShow = () => {}
+
     render() {
+
+       
+
         return (
             <div className="inbox-wrapper">
 
                 <div className="inbox-title-wrapper">
                     <span className="inbox-title">Inbox</span>
-                
-                <div className="inbox-title-actions">
-                    <span className="inbox-toggle-mode">
-                        <a href="" className="active"> Open (0)</a>
-                        /
+
+                    <div className="inbox-title-actions">
+                        <span className="inbox-toggle-mode">
+                            <a href="" className="active"> Open (0)</a>
+                            /
                 <a href="" className="inbox-all-updates">All Updates</a>
 
-                    </span>
+                        </span>
+                    </div>
                 </div>
-                </div>
-                
-                
-                
+
+
+
                 <div className="middle-space-wrapper">
                     <div className="middle-space-wall">
-                    <div className="inbox-middle-default">
-                    <br></br>
-                        <i className="material-icons">beach_access</i>
-
-                    </div>
                         <div className="posts-list">
-                        {/* <PostComponent></PostComponent> */}
+                            <DefaultInbox></DefaultInbox>
+                            
+                        
+
                         </div>
 
                     </div>
