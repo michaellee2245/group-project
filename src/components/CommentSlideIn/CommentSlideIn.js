4a042905004ff754149037c65adba9e92a5d8bb7
McKay Nilsson <mckay.nilsson@gmail.com>
2019-02-16 14:03:31 -0700

    pull from master

---
+++ src/components/CommentSlideIn/CommentSlideIn.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './comment-slide-in.scss';
+
+class CommentSlideIn extends Component {
+    render(){
+        return(
+            <div className="slide-in-container">
+                Slide In
+            </div>
+        )
+    }
+}
+
+export default CommentSlideIn;
