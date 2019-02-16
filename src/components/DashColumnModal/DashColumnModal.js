de8683288ec74d7778e7c44814a44b403265145b
McKay Nilsson <mckay.nilsson@gmail.com>
2019-02-16 12:46:59 -0700

    changed id's to classes within modal

--- src/components/DashColumnModal/DashColumnModal.js
+++ src/components/DashColumnModal/DashColumnModal.js
@@ -6,47 +6,61 @@ import 'react-day-picker/lib/style.css';
 
 class DashColumnModal extends Component {
 
+    state = {
+        priority: false,
+        status: true,
+        date: false,
+        person: false,
+        selectedDay: null,
+        index: true
+
+    }
+
     handleDayClick = (day, { selected }) => {
         this.setState({
-          selectedDay: selected ? undefined : day,
+            selectedDay: selected ? undefined : day,
         });
-      }
+    }
 
     render() {
 
-        const { priority, status, date, person } = this.props;
+        const { priority, status, date, person } = this.state;
 
         return (
-            <div className="column-modal-container">
+            <div>
 
-                {priority ? (
+                    <div className="column-modal-container">
 
-                    <div className="priority-container">
-                        <div className="column-modal-button" id="high-priority"> High </div>
-                        <div className="column-modal-button" id="medium-priority"> Medium </div>
-                        <div className="column-modal-button" id="low-priority"> Low </div>
+                        {priority ? (
+
+                            <div className="priority-container">
+                                <div className="column-modal-button high-priority"> High </div>
+                                <div className="column-modal-button medium-priority"> Medium </div>
+                                <div className="column-modal-button low-priority"> Low </div>
+                            </div>
+
+                        ) : null}
+
+                        {status ? (
+
+                            <div className="status-container">
+                                <div className="column-modal-button done-status"> Done </div>
+                                <div className="column-modal-button in-progress-status" > In Progress </div>
+                                <div className="column-modal-button on-hold-status"> On Hold </div>
+                            </div>
+
+                        ) : null}
+
+                        {date ? (
+                            <div className="date-container">
+                                <DayPicker
+                                    selectedDays={this.state.selectedDay}
+                                    onDayClick={this.handleDayClick}
+                                />
+                            </div>
+                        ) : null}
+
                     </div>
-
-                ) : null}
-
-                {status ? (
-
-                    <div className="status-container">
-                        <div className="column-modal-button" id="done-status"> Done </div>
-                        <div className="column-modal-button" id="in-progress-status"> In Progress </div>
-                        <div className="column-modal-button" id="on-hold-status"> On Hold </div>
-                    </div>
-
-                ) : null}
-
-                {date ? (
-                    <div className="date-container">
-                        <DayPicker 
-                            selectedDays={this.props.selectedDay}
-                            onDayClick={this.handleDayClick}    
-                        />
-                    </div>
-                ) : null}
 
             </div>
 
