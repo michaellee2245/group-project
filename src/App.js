57992ef3b43ce919f0c4762133046e7370278a22
JHall4475 <jhall4475@yaoo.com>
2019-02-16 12:23:39 -0700

    inbox push

--- src/App.js
+++ src/App.js
@@ -4,29 +4,21 @@ import './App.css';
 import DashboardLanding from './views/dashboard/DashboardLanding';
 import { ConnectedRouter } from 'connected-react-router';
 import {Switch,Route} from 'react-router-dom';
-import Marketing from './views/marketing/Marketing';
-import TopNavBar from './components/TopNavBar/TopNavBar';
-import Inbox from '../src/components/Inbox/Inbox'
-import InboxPosts from '../src/components/InboxPosts/InboxPosts'
-
+import Marketing from './views/marketing/Marketing'
+import WhyUs from './views/marketing/MarketWhyUs/MarketWhyUs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <ConnectedRouter history={ history }>
+        <ConnectedRouter history={history}>
           <Switch>
             <Route path = '/marketing' component = {Marketing} />
             <Route path="/dashboard" component = {DashboardLanding} />    
             <Route path="/why-us" component = {WhyUs} />    
             <Route component={Marketing} />
           </Switch>
-        </ConnectedRouter> */}
-
-        <Inbox/>
-      
-       
-        
+        </ConnectedRouter>
       </div>
 
     );
