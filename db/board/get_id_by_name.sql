c8afb983ba04f282ec5ceae3cf55b47868611b65
joshparkerj <superfakek@gmail.com>
2019-02-20 18:11:31 -0700

    board post changes

--- db/board/get_id_by_name.sql
+++ db/board/get_id_by_name.sql
@@ -1,4 +1,15 @@
 /* get board id by name. used when generating a new board */
 
-SELECT id FROM board WHERE name = $1
+SELECT
+  board.id AS id,
+  board.name AS name,
+  board.description AS description,
+  board.team AS team_id,
+  board.owner AS owner_id,
+  owner.name AS owner,
+  owner.profile_pic AS owner_pic,
+  team.name AS team FROM board
+JOIN person AS owner ON owner.id = board.owner
+JOIN team ON board.team = team.id
+FROM board WHERE name = $1
 ORDER BY ts DESC;
