5ac6277f1dfb90bca66d297ccf673095fb87ec1a
McKay Nilsson <mckay.nilsson@gmail.com>
2019-02-23 10:39:45 -0700

    added logo to top nav

--- src/components/TopNavBar/TopNavBar.js
+++ src/components/TopNavBar/TopNavBar.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
-import {logout} from '../../redux/actions'
+import { connect } from 'react-redux'
+import { logout } from '../../redux/actions'
 import './TopNavBar.scss'
 
 class TopNavBar extends Component {
-    
+
     state = {
         profileHidden: false,
     }
 
     handleClick = () => {
-        this.setState ({profileHidden: !this.state.profileHidden})
+        this.setState({ profileHidden: !this.state.profileHidden })
     }
     handleAdmin = () => {
         this.props.page('/dashboard/admin')
@@ -19,72 +19,77 @@ class TopNavBar extends Component {
     render() {
         return (
             <div className="navbar-wrapper">
-                <div className = 'first-three'>
-                    <div className="navbar-tab">
-                        <a href="#" className="text">
-                            <div className="add-member-span">
-                                <div className="dropdown-notifs">
-                                    <i className="material-icons">notifications_none</i>
-                                    <div className="dropdown-content">
-                                        Notifications
+                <div className="logo-tab-wrapper">
+                    <div
+                        className="dash-nav-logo"
+                    />
+                    <div className='first-three'>
+                        <div className="navbar-tab">
+                            <a href="#" className="text">
+                                <div className="add-member-span">
+                                    <div className="dropdown-notifs">
+                                        <i className="material-icons">notifications_none</i>
+                                        <div className="dropdown-content">
+                                            Notifications
+                                    </div>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="navbar-tab">
-                        <a href="#" className="text">
-                            <div className="dropdown-people">
-                                <i className="material-icons">people_outline</i>
-                                <div className="dropdown-content">
-                                    Team Members
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="navbar-tab">
-                        <input placeholder="Search Everything ..." className="search-bar">
-                        </input>
-                            <a href = 'https://google.com'>
-                        <div className="search-icon">
-                            <i className="material-icons">search</i>
-                        </div>
                             </a>
-                        
-                        
+                        </div>
+                        <div className="navbar-tab">
+                            <a href="#" className="text">
+                                <div className="dropdown-people">
+                                    <i className="material-icons">people_outline</i>
+                                    <div className="dropdown-content">
+                                        Team Members
+                                </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="navbar-tab">
+                            <input placeholder="Search Everything ..." className="search-bar">
+                            </input>
+                            <a href='https://google.com'>
+                                <div className="search-icon">
+                                    <i className="material-icons">search</i>
+                                </div>
+                            </a>
+
+
+                        </div>
                     </div>
                 </div>
 
                 <div className='last-two'>
-                <div className="navbar-tab">
-                    <a href="#" className="text">
-                        <div className="add-member-span">
-                            <i className="material-icons">add_circle_outline</i>
-                            <p> Invite Team Members </p>
-                        </div>
-                    </a>
-                </div>
+                    <div className="navbar-tab">
+                        <a href="#" className="text">
+                            <div className="add-member-span">
+                                <i className="material-icons">add_circle_outline</i>
+                                <p> Invite Team Members </p>
+                            </div>
+                        </a>
+                    </div>
 
-                <div className="navbar-tab">
-                    {/* <a href="#" className="text"> */}
-                        <div onClick = {this.handleClick}  className="add-member-span">
+                    <div className="navbar-tab">
+                        {/* <a href="#" className="text"> */}
+                        <div onClick={this.handleClick} className="add-member-span">
                             <i className="material-icons">face</i>
-                            <p className = 'user-profile-menu' > UserProfile</p>
-                            {this.state.profileHidden? 
-                            (
-                            <div>
-                                <ul className = 'user-profile-menu-list'>
-                                    <li>Profile</li>
-                                    <li onClick = {this.handleAdmin}>Admin </li>
-                                    <li>Recycle Bin </li>
-                                    <li onClick = {this.props.logout}>Logout</li>
-                                </ul>
-                            </div>    
-                            ): (null)
+                            <p className='user-profile-menu' > UserProfile</p>
+                            {this.state.profileHidden ?
+                                (
+                                    <div>
+                                        <ul className='user-profile-menu-list'>
+                                            <li>Profile</li>
+                                            <li onClick={this.handleAdmin}>Admin </li>
+                                            <li>Recycle Bin </li>
+                                            <li onClick={this.props.logout}>Logout</li>
+                                        </ul>
+                                    </div>
+                                ) : (null)
                             }
                         </div>
-                    {/* </a> */}
-                </div>
+                        {/* </a> */}
+                    </div>
                 </div>
             </div>
         )
@@ -93,4 +98,4 @@ class TopNavBar extends Component {
 
 }
 
-export default connect(null,{logout})(TopNavBar);
+export default connect(null, { logout })(TopNavBar);
