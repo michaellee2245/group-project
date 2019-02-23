c8cf849ceaf15e80649c22f85c05aa1b17f224b8
JHall4475 <jhall4475@yaoo.com>
2019-02-23 11:56:24 -0700

    feb 23 changes

--- src/components/Inbox/DirectMessages.js
+++ src/components/Inbox/DirectMessages.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './inboxPosts.scss';
+import './directMessages.scss';
 import axios from 'axios';
 
-class InboxPosts extends Component {
+class DirectMessages extends Component {
 
 
     markAsRead = () => {
@@ -38,16 +38,16 @@ class InboxPosts extends Component {
                                 <img className="profile-pic" src={authorPic} alt="None"></img>
                             </a>
                             <div className="title">
-                                <a className="user-name" href="Profile-Name">{author}</a>
-                                <div className="post-board-link">
+                                <a className="user-name" href="Profile-Name">Direct message from {author}</a>
+                                {/* <div className="post-board-link">
                                     <a href="actual-post-board-link" className="router-link">
                                         <i className="material-icons">sort</i>
-                                        {boardName}
+                                        Direct message
                                     </a>
-                                    <span id="divider"> > </span>
+                                    <span id="divider"> from </span>
                                     <a href="task-name" className="router-link">{taskName}</a>
-                                </div>
-                            </div>
+                                </div> */}
+                            </div> 
                         </div>
                         <div className="post-top-right-wrapper">
                             {/* <div className="post-time-wrapper">
@@ -108,4 +108,4 @@ class InboxPosts extends Component {
     }
 }
 
-export default InboxPosts;
+export default DirectMessages;
--- src/components/Inbox/Inbox.js
+++ src/components/Inbox/Inbox.js
@@ -3,65 +3,69 @@ import './inbox.scss'
 import DefaultInbox from './DefaultInbox';
 import InboxPosts from '../InboxPosts/InboxPosts'
 import axios from 'axios';
+import DirectMessages from './DirectMessages';
 
 
 class Inbox extends Component {
 
     state = {
         directMessages: [],
-        comments:[],
+        comments: [],
 
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getDirectMessages()
         this.getComments()
     }
 
-    getDirectMessages= () => {
+    getDirectMessages = () => {
         axios.get(`/api/message`)
-        .then((response) => {
-            console.log("these are the messages", response)
-            this.setState({directMessages: response.data})
-        })
+            .then((response) => {
+                console.log("these are the messages", response)
+                this.setState({ directMessages: response.data })
+            })
     }
-    
+
     getComments = () => {
         axios.get(`/api/comment`)
-        .then((comments) => {
-            console.log("these are the comments", comments)
-            this.setState({comments: comments.data})
-        })
+            .then((comments) => {
+                console.log("these are the comments", comments)
+                this.setState({ comments: comments.data })
+            })
     }
 
-    countComments =() => {
-      return  this.state.comments.filter(comment => {
-          return !comment.read
-      }).length
+    countComments = () => {
+        return this.state.comments.filter(comment => {
+            return !comment.read
+        }).length
     }
 
-    markCommentRead= (commentID) => {
-        
+    markCommentRead = (commentID) => {
+
         const comments = this.state.comments
         console.log("comments and index", comments, commentID)
-       const comment = comments.find(comment=>comment.id === commentID)
-       comment.read = !comment.read
-        this.setState({comments: [...comments]})
+        const comment = comments.find(comment => comment.id === commentID)
+        comment.read = !comment.read
+        this.setState({ comments: [...comments] })
     }
 
     showAllUpdates = () => {
-        return  this.state.comments.map(this.renderInboxPosts)
+        return this.state.comments.map(this.renderInboxPosts)
     }
 
-    showOpenUpdates= () => {
-        return this.state.comments.filter( comment => {
+    showOpenUpdates = () => {
+        return this.state.comments.filter(comment => {
             return !comment.read
-            
+
         }).map(this.renderInboxPosts)
     }
+    showDirectMessages = () => {
+        return this.state.directMessages.map(this.renderDirectMessages)
+    }
 
-    renderInboxPosts = (comment, index)=>{
-        return(
+    renderInboxPosts = (comment, index) => {
+        return (
             <InboxPosts
                 author={comment.author}
                 authorPic={comment.author_pic}
@@ -73,21 +77,41 @@ class Inbox extends Component {
                 readFunction={this.markCommentRead}
                 commentRead={comment.read}
                 readCount={comment.read_count}
+                likesCount={comment.like_count}
             />
-         ) }
+        )
+    }
 
-         handleAllUpdates=(event)=> {
-             event.preventDefault()
-             this.setState({allUpdates: true})
-         }
-        handleOpenUpdates=(event)=>{
-            event.preventDefault()
-            this.setState({allUpdates:false})
-        }
+    renderDirectMessages=(message)=>{
+        return(
+            <DirectMessages
+                author={message.author}
+                authorPic={message.author_pic}
+                content={message.content}
+
+            ></DirectMessages>
+        )
+    }
+
+
+
+    handleAllUpdates = (event) => {
+        event.preventDefault()
+        this.setState({ allUpdates: true, showDirectMessages: false })
+
+    }
+    handleOpenUpdates = (event) => {
+        event.preventDefault()
+        this.setState({ allUpdates: false, showDirectMessages: false })
+    }
+    handleDirectMessages = (event) => {
+        event.preventDefault()
+        this.setState({ allUpdates: false, showDirectMessages: true })
+    }
 
     render() {
 
-       
+
 
         return (
             <div className="inbox-wrapper-component">
@@ -99,7 +123,9 @@ class Inbox extends Component {
                         <span className="inbox-toggle-mode">
                             <a href="" className="active" onClick={this.handleOpenUpdates}> Open ({this.countComments()}) </a>
                             /
-                <a href="" className="inbox-all-updates" onClick={this.handleAllUpdates}> All Updates</a>
+                <a href="" className="inbox-all-updates" onClick={this.handleAllUpdates}> All Updates </a>
+                            /
+                <a href="" className="inbox-direct-messages" onClick={this.handleDirectMessages}> DMs</a>
 
                         </span>
                     </div>
@@ -111,12 +137,16 @@ class Inbox extends Component {
                     <div className="middle-space-wall">
                         <div className="posts-list">
 
-                        {this.countComments() > 0 && !this.state.allUpdates || this.state.comments.length > 0 && this.state.allUpdates ?
-                        '' :
-                        (<DefaultInbox></DefaultInbox>)
-                        }
-                        {this.state.allUpdates ?  this.showAllUpdates() : this.showOpenUpdates()}
-            
+                            {this.countComments() > 0 && !this.state.allUpdates || this.state.comments.length > 0 && this.state.allUpdates ?
+                                '' :
+                                (<DefaultInbox/>)
+                            }
+                            {this.state.allUpdates ?
+                                this.showAllUpdates() :
+                                this.state.showDirectMessages ?
+                                    this.showDirectMessages() :
+                                    this.showOpenUpdates()}
+
                         </div>
 
                     </div>
