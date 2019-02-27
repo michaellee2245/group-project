840cb25a0110d402bf5d3feba2646f28d11815cb
Michael Lee <michaellee2245@gmail.com>
2019-02-26 17:07:07 -0700

    updated carousel content and styles

--- src/components/Carousel/slides.js
+++ src/components/Carousel/slides.js
@@ -9,6 +9,13 @@ import sales from './images/sales-pipeline.jpg';
 import sprints from './images/agile-sprints.jpg';
 import becky from './images/becky.jpg';
 import samantha from './images/samantha.jpg';
+import dani from './images/dani.jpg';
+import jessica from './images/jessica.jpg';
+import sarah from './images/sarah.jpg';
+import john from './images/john.jpg';
+import jeremy from './images/jeremy.jpg';
+import brixton from './images/brixton.jpg';
+import colton from './images/colton.jpg';
 
 const slides = [
     {
@@ -25,7 +32,7 @@ const slides = [
         color: "blue",
         title: "Roadmap Management",
         image: roadmap,
-        quote: "This is another quote that will make people think and they'll want to buy and use our product",
+        quote: "This is another quote that will make people think and they'll want to buy and use our product.",
         quoteName: "Samantha from Sales Coordination",
         quoteImage: samantha,
     },
@@ -34,63 +41,63 @@ const slides = [
         color: "yellow",
         title: "Content Calendar",
         image: content,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "I've never been more productive at work than when my company started using this software!",
+        quoteName: "John with State Farm",
+        quoteImage: john,
     },
     {
         upperText: "HR",
         color: "purple",
         title: "HR Management",
         image: hr,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "Never thought we'd be able to use this software for HR, but since we started I can't think of using anything else!",
+        quoteName: "Jeremy",
+        quoteImage: jeremy,
     },
     {
         upperText: "work",
         color: "green",
         title: "Project Management",
         image: work,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "I always know where I'm at on my various projects and can easily check in with others to see how their projects are going.",
+        quoteName: "Dani with Cricut",
+        quoteImage: dani,
     },
     {
         upperText: "team",
         color: "red",
         title: "Team Tasks",
         image: team,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "It can do so many things that it can be tailored to each and every team.",
+        quoteName: "Brixton with Adobe",
+        quoteImage: brixton,
     },
     {
         upperText: "orders",
         color: "cyan",
         title: "Orders",
         image: orders,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "The map view makes it easy to track all of our shipments. This software really is a one-stop shop!",
+        quoteName: "Colton with Minamol",
+        quoteImage: colton,
     },
     {
         upperText: "sales",
         color: "orange",
         title: "Sales Pipeline",
         image: sales,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "Our sales numbers have increased 25% since we started using this software. It really helps us follow up on leads.",
+        quoteName: "Jessica with Wavetronix",
+        quoteImage: jessica,
     },
     {
         upperText: "sprints",
         color: "blue",
         title: "Agile Sprints",
         image: sprints,
-        quote: "This is a special quote and is very meaningful indeed that it will influence people.",
-        quoteName: "Becky from Technical Services",
-        quoteImage: becky,
+        quote: "When we need something done quickly this is the first place I go to set up assignments.",
+        quoteName: "Sarah with Domo",
+        quoteImage: sarah,
     },
 ];
 export default slides;
